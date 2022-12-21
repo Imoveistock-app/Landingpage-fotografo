b4bf58977c5e67f754cf8c42af335a40234a83a4
Iago Félix <iagosilvafelix@gmail.com>
2022-12-21 18:59:51 -0300

    footer/env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  payloadKey: '57b0e476-142c-4762-8547-140b65cb1e9c',
+  apis: {
+    imoveistock: 'https://imoveistockapi.mustblockchain.com/',
+    // imoveistock: 'http://localhost:3000/',
+  }
 };
